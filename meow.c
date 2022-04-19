350b89cfc5b1a838cde46a404dd5b694ad5809ac
Thien Ly Buu Huynh <45794429+xiunhoxiu@users.noreply.github.com>
2022-04-19 15:02:12 +0200

    added meow func

--- meow.c
+++ meow.c
@@ -1,9 +1,15 @@
 #include <stdio.h>
 
+
+void meow(void) // return nothing and takes no input, purpose: visual effects
+{
+    printf("meow\n");
+}
+
 int main(void)
 {
     for (int i = 0; i < 3; i++)
     {
-        printf("meow\n");
+        meow();
     }
 }
