a9c2ac0ca337fc5822167cf8b4d779cde2665adb
Thien Ly Buu Huynh <45794429+xiunhoxiu@users.noreply.github.com>
2022-04-19 15:05:40 +0200

    structuring

--- meow.c
+++ meow.c
@@ -1,10 +1,8 @@
 #include <stdio.h>
 
 
-void meow(void) // return nothing and takes no input, purpose: visual effects
-{
-    printf("meow\n");
-}
+void meow(void); // declare a function at the top
+
 
 int main(void)
 {
@@ -12,4 +10,10 @@ int main(void)
     {
         meow();
     }
+}
+
+
+void meow(void) // return nothing and takes no input, purpose: visual effects, defining a func after main.
+{
+    printf("meow\n");
 }
