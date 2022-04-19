d2eb592e2ad9dd97f8c534d3ddaa1c600f69c28d
Thien Ly Buu Huynh <45794429+xiunhoxiu@users.noreply.github.com>
2022-04-19 15:14:36 +0200

    updated meow.c

--- meow.c
+++ meow.c
@@ -1,19 +1,19 @@
 #include <stdio.h>
 
 
-void meow(void); // declare a function at the top
+void meow(int n); // declare a function at the top
 
 
 int main(void)
 {
-    for (int i = 0; i < 3; i++)
-    {
-        meow();
-    }
+    meow(3);
 }
 
 
-void meow(void) // return nothing and takes no input, purpose: visual effects, defining a func after main.
+void meow(int n) //void returns nothing, defining a func after main.
 {
-    printf("meow\n");
+    for (int i = 0; i < n; i++)
+    {
+        printf("meow\n");
+    }
 }
