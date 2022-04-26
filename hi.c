b87e88d7b7be92a432a7d02aa26b3082ce931197
Thien Ly Buu Huynh <45794429+xiunhoxiu@users.noreply.github.com>
2022-04-26 10:35:03 +0200

    modified hi.c, deleted hi1.c

--- hi.c
+++ hi.c
@@ -1,12 +1,22 @@
 #include <stdio.h>
+#include <cs50.h>
 
 int main(void)
 {
     char c1 = 'H';
     char c2 = 'I';
     char c3 = '!';
+    string s = "HI!";
+    string t = "BYE!";
+    
+    
     
     printf("%i %i %i\n", c1, c2 ,c3);
     // printing ASCII values (Unicode) - converting chars to ints.
     // To explicitly convert - use typecasting ..., (int) c1, (int) c2 , ... etc.
+    
+    printf("%i %i %i\n", s[0], s[1], s[2]); // a string is an array of chars.
+    printf("%s\n", s);
+    printf("%s\n", t);
+
  }
--- hi1.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <cs50.h>
-
-int main(void)
-{
-    string s = "HI!";
-    printf("%i %i %i\n", s[0], s[1], s[2]);
-    // a string is an array of chars.
- }
