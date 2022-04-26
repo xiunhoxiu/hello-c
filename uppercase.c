e37c5ff3483b71c90b7ea823ed77c4b135633cb9
Thien Ly Buu Huynh <45794429+xiunhoxiu@users.noreply.github.com>
2022-04-26 11:54:56 +0200

    from islower() to toupper()

--- uppercase.c
+++ uppercase.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <ctype.h>
 #include <cs50.h>
 #include <string.h>
 
@@ -6,6 +7,34 @@ int main (void)
 {
     string s = get_string("Before: ");
     printf("After:  ");
+
+    for (int i = 0, n = strlen(s); i < n; i++)
+    {
+        printf("%c", toupper(s[i]));   
+    }
+    printf("\n");
+
+    /* for (int i = 0, n = strlen(s); i < n; i++)
+    {
+
+        //islower returns non-zero int if lowercase and 0 (false) if not a lowercase.
+        // this works bc. it uses any other value to represent true. If lower it prints out the character minus 32. 
+        // can also be written islower(s[i]) != 0
+       
+        if(islower(s[i])) 
+        {
+            printf("%c", toupper(s[i])); 
+           
+        }
+        else
+        {
+            printf("%c", s[i]);
+        } 
+        
+    }
+    printf("\n"); */
+    
+/*  
     
     for (int i = 0, n = strlen(s); i < n; i++)
     {
@@ -20,8 +49,9 @@ int main (void)
         else
         {
             printf("%c", s[i]);
-        }
+        } 
         
     }
     printf("\n");
+*/
 }
