c615f2fc9aa83689adcaf8bb464fa883a0dfac4c
Thien Ly Buu Huynh <45794429+xiunhoxiu@users.noreply.github.com>
2022-05-03 11:34:34 +0200

    added iteration.c

--- phonebook.c
+++ phonebook.c
@@ -2,17 +2,39 @@
 #include <stdio.h>
 #include <string.h>
 
+typedef struct
+{
+    string name;
+    string number;
+
+}
+person;
+
+
+
 int main(void)
 {
-    string names[] = {"Carter", "David"};
-    string numbers[] = {"+1 617-495-1000", "+1-949-468-2750"}; 
-    // phone numbers corresponses to the names array respectively
+ 
+    person people[2];
+
+    people[0].name = "Carter";
+    people[0].number = "+1 617-495-1000";
+
+    people[1].name = "David";
+    people[1].number = "+1-949-468-2750";
+
+    /*  
+        string names[] = {"Carter", "David"};
+        string numbers[] = {"+1 617-495-1000", "+1-949-468-2750"}; 
+        // assuming phone numbers corresponses to the names array respectively
+    */
 
     for (int i = 0; i < 2; i++)
     {
-        if (strcmp(names[i], "David") == 0)
+        if (strcmp(people[i].name, "David") == 0)
         {
-            printf("Found %s\n", numbers[i]);
+            printf("Found %s\n", people[i].number);
+            // printf("Found %s\n", numbers[i]);
             return 0;
         }
     }
