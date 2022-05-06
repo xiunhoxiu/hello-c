5cccf79e4dc80220d3e398375892c23e30b9b0c5
Thien Ly Buu Huynh <45794429+xiunhoxiu@users.noreply.github.com>
2022-05-06 11:36:09 +0200

    pointers and addresses

--- week4/address.c
+++ week4/address.c
@@ -3,5 +3,9 @@
 int main(void)
 {
     int n = 50;
-    printf("%i\n", n);
+    int *p = &n; // declare a pointer
+    printf("%p\n", p);
+    printf("%i\n", n);  
+    printf("%i\n", *p);  //dereference operator (go to that address) but will show the value in this location.
+    
 }
