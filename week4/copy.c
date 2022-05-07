4e4c81258fc8d64c6e62a3c42099045218034a97
Thien Ly Buu Huynh <45794429+xiunhoxiu@users.noreply.github.com>
2022-05-07 16:36:18 +0200

    two different pointers storing same address

--- week4/copy.c
+++ week4/copy.c
@@ -1,8 +1,15 @@
 #include <stdio.h>
 #include <cs50.h>
+#include <ctype.h>
 #include <string.h>
 
 int main (void)
 {
+    string s = get_string("s: ");
 
+    string t = s;
+    t[0] = toupper(t[0]);
+
+    printf("s: %s\n", s);
+    printf("t: %s\n", t);
 }
