ae3c20021518f5a883ce36466cdf001bb9de472c
Thien Ly Buu Huynh <45794429+xiunhoxiu@users.noreply.github.com>
2022-05-07 16:58:06 +0200

    malloc() and strcpy() used to copy

--- week4/copy.c
+++ week4/copy.c
@@ -2,12 +2,15 @@
 #include <cs50.h>
 #include <ctype.h>
 #include <string.h>
+#include <stdlib.h>
 
 int main (void)
 {
-    string s = get_string("s: ");
+    char *s = get_string("s: ");
+    char *t = malloc(strlen(s+1)); // + 1 for the addition null character.
 
-    string t = s;
+    strcpy(t,s); // copying from right to left. s is the source on the second arguemnt.
+
     t[0] = toupper(t[0]);
 
     printf("s: %s\n", s);
