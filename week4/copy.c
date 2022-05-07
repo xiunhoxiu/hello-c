cf57cfd549c820c0d23fb48118340cd5950d50af
Thien Ly Buu Huynh <45794429+xiunhoxiu@users.noreply.github.com>
2022-05-07 17:25:08 +0200

    added conditions to capture problems that might occur

--- week4/copy.c
+++ week4/copy.c
@@ -8,13 +8,21 @@ int main (void)
 {
     char *s = get_string("s: ");
     char *t = malloc(strlen(s+1)); // + 1 for the addition null character.
-
+    if (t == NULL)
+    {
+        return 1; // if there is a problem
+    }
+    
     strcpy(t,s); // copying from right to left. s is the source on the second arguemnt.
 
-    t[0] = toupper(t[0]);
+    if(strlen(t) > 0) // what if there's no /0?
+    {
+         t[0] = toupper(t[0]); //risky.
+    }
 
     printf("s: %s\n", s);
     printf("t: %s\n", t);
 
     free(t); // s gets freed by the CS50 library.
+    return 0;
 }
