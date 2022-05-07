b05368aa3384db940963775dde7cfcc45d6c6000
Thien Ly Buu Huynh <45794429+xiunhoxiu@users.noreply.github.com>
2022-05-07 19:43:44 +0200

    added scanf.c

--- week4/scanf.c
+++ week4/scanf.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(void)
 {
-    int x;
-    printf("x: ");
-    scanf("%i\n", &x);
-    printf("x: %in", x);
+    char *s;
+    printf("s: ");
+    scanf("%s", s);
+    printf("s: %s\n", s);
 }
