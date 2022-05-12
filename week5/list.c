edb09ceadf75bac3a92acb82020c0b1972fde9cf
Thien Ly Buu Huynh <45794429+xiunhoxiu@users.noreply.github.com>
2022-05-12 16:51:05 +0200

    linked list

--- week5/list.c
+++ week5/list.c
@@ -1,50 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef struct node
+{
+    int number;
+    struct node *next;
+}
+node;
+
+
 int main (void)
 {
-    int *list = malloc(3 * sizeof(int)); // dynamic allocating - resizing possible. 
-    if(list == NULL)
+    // List of size
+    node *list = NULL;
+
+    // Add a number to list
+    node *n = malloc(sizeof(node));
+    if (n == NULL)
     {
-        return 1;
+        return 1; // quit the program
     }
+    n -> number = 1;
+    n -> next = NULL;
 
-    // Assign three numbers to that array
-    list[0] = 1;
-    list[1] = 2;
-    list[2] = 3;
-
-    // Time passes
-
-    // Allocate new array of size 4
-    int *tmp = malloc(4*sizeof(int)); 
-    if(tmp == NULL)
-    {
-        free(list);
-        return 1;
-    }
-
-    // Copy numbers from old array into new array
-    for (int i = 0; i < 3; i ++)
-    {
-        tmp[i] = list[i]; 
-    }
-    // Add fourth number to new array
-    tmp[3] = 4; 
-
-    // free old array - "list of memory (address)" and then change it's value!
-    free(list); 
-
-    // Remember new array
-    list = tmp; 
-
-    // print new array
-    for( int i = 0; i < 4; i++)
-    {
-        printf("%i\n", list[i]); 
-    }
-
-    // free the new array/list at the end. All heap blocks were freed -- no leaks are possible.
-    free(list); 
-    return 0; 
+    // Update list to point to new node
+    list = n;
+    
 }
