7c63b8d5e14f39bf8fe9285ae407b675baa1cb0f
Thien Ly Buu Huynh <45794429+xiunhoxiu@users.noreply.github.com>
2022-05-12 17:27:35 +0200

    linked list, nodes, malloc and freeing

--- week5/list.c
+++ week5/list.c
@@ -26,4 +26,49 @@ int main (void)
     // Update list to point to new node
     list = n;
     
+    // Add a number to list
+    n = malloc(sizeof(node));
+    if(n == NULL)
+    {
+        free (list);
+        return 1;
+    }
+    n->number = 2;
+    n->next = NULL;
+
+    list->next = n;
+
+
+    // Add a number to list
+    n = malloc(sizeof(node));
+    if (n == NULL)
+    {   
+        free(list->next);
+        free(list);
+        return 1;
+    }
+    n->number = 3; 
+    n->next = NULL;
+    list->next->next = n;
+
+
+    // print numbers
+    /*  pointer arithmetic no longer comes into play 
+        as it is us who are stitching together the datastruture in memory.
+        [] cannot be used anymore as nodes are randomly located all over. */ 
+
+    for (node *tmp = list; tmp != NULL; tmp = tmp->next)
+    {
+        printf("%i\n", tmp->number);
+    }
+    // you never have to free pointers - only free addresses that are returned to you by malloc.
+
+    // Free list
+    while (list != NULL)
+    {
+        node *tmp = list->next; // different tmp bc. it is in a different scope.
+        free(list); // freeing the address in list, the address of number 1 node.
+        list = tmp; // update the list to be tmp.
+    }
+    return 0;
 }
