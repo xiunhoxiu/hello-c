9eef52a1d8bcb196a436fda95f31935e0434cae5
Thien Ly Buu Huynh <45794429+xiunhoxiu@users.noreply.github.com>
2022-05-13 23:39:37 +0200

    blur image

--- week6/blur.py
+++ week6/blur.py
@@ -1,7 +1,5 @@
 from PIL import Image, ImageFilter
 
 before = Image.open("bridge.bmp")
-after = before.filter(ImageFilter.Boxblur(1))  # blur in box format, like 1 px above, below, left and right.
+after = before.filter(ImageFilter.BoxBlur(10))  
 after.save("out.bmp")
-
-
