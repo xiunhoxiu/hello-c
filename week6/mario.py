c7e93f03338044c78140c59d25db6ef0f9ba2606
Thien Ly Buu Huynh <45794429+xiunhoxiu@users.noreply.github.com>
2022-05-14 04:20:21 +0200

    modified mario.py

--- week6/mario.py
+++ week6/mario.py
@@ -6,9 +6,13 @@ def main():
 
 def get_height():
     while True:
-        n = int(input("Height: "))
-        
-        if n > 0: 
-            return n
+        try:
+            n = int(input("Height: "))
+            
+            if n > 0: 
+                return n
+                
+        except ValueError:
+            print("That's not a integer!")
 
 main()
