157d77c7f13d2b6331a62ff050fc0599fdab94e6
Thien Ly Buu Huynh <45794429+xiunhoxiu@users.noreply.github.com>
2022-05-14 04:12:55 +0200

    added mario.py

---
+++ week6/mario.py
@@ -0,0 +1,17 @@
+from cs50 import get_int
+
+
+def main():
+    height = get_height()
+    for i in range(height):
+        print("#")
+
+
+def get_height():
+    while True:
+        n = get_int("Height: ")
+        
+        if n > 0: 
+            return n
+
+main()
