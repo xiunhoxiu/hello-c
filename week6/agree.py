91ea7a7d446f5909600d50e19cc08ad475aeafb2
Thien Ly Buu Huynh <45794429+xiunhoxiu@users.noreply.github.com>
2022-05-14 03:32:04 +0200

    .lower()

--- week6/agree.py
+++ week6/agree.py
@@ -2,9 +2,9 @@ from cs50 import get_string
 
 s = get_string("Do you agree? ")
 
-if s == "Y" or s == "y":
+if s.lower() in ["y", "yes"]:
     print("Agreed")
-elif s == "N" or s == "n":
+elif s.lower() in ["n", "no"]:
     print("Not agreed.")
 
 
