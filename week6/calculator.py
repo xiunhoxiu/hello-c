0422838f82da8fec3a703104b2764e424145a2ea
Thien Ly Buu Huynh <45794429+xiunhoxiu@users.noreply.github.com>
2022-05-14 02:30:11 +0200

    modified calculator.c

--- week6/calculator.py
+++ week6/calculator.py
@@ -1,16 +1,5 @@
 import cs50
 
-x = get_int("x: ")
-y = get_int("y; ")
+x = cs50.get_int("x: ")
+y = cs50.get_int("y; ")
 print(x + y)
-
-""" Traceback of all code that got executed: 
-    Stack trace.
-    Python allows to namespace the functions that come from libraries
-    Collision happens when you use another library
-    that happens to contain same function names. 
-    The compiler would not know how to link them correctly.
-    In python and other languages support for namespaces,
-    where you can isolate variables and function to their own namespace,
-    like having their own container in memory.
-"""
