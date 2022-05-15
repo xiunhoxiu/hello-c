92188a8c152009313088cb176bfcc6fef49f3dee
Thien Ly Buu Huynh <45794429+xiunhoxiu@users.noreply.github.com>
2022-05-15 15:38:09 +0200

    foo bar baz

--- week6/argv.py
+++ week6/argv.py
@@ -1,11 +1,5 @@
-# to accesss argument vector, you need to import it, unlike in C
 from sys import argv
 
-if len(argv) == 2:
-    print(f"hello, {argv[1]}")
-else:
-    print("hello, world")
+for arg in argv:
+    print(arg)
 
-# CLI: python argv.py Xiu
-# the word python does not apppear in the argv list.
-# The name of the python program is in [0], whereas the name Xiu is in [0]
