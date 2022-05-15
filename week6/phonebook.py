1fa966692311c08d054707d28b1b54654c12c354
Thien Ly Buu Huynh <45794429+xiunhoxiu@users.noreply.github.com>
2022-05-15 17:21:26 +0200

    with keyword

--- week6/phonebook.py
+++ week6/phonebook.py
@@ -1,13 +1,11 @@
 import csv
 from cs50 import get_string
 
-
-file = open("phonebook.csv", "a")  # "a" - append format, adds to the bottom.
-
 name = get_string("Name: ")
 number = get_string("Number: ")
 
-writer = csv.writer(file)
-writer.writerow([name, number])
+# with keyword takes care of memory leak.
+with open("phonebook.csv", "a") as file:
 
-file.close()
+    writer = csv.writer(file)
+    writer.writerow([name, number])
