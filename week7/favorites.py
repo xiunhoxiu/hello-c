c662bd72d4577b5ef4770c0f27b7879536f3f9cf
Thien Ly Buu Huynh <45794429+xiunhoxiu@users.noreply.github.com>
2022-05-16 11:29:18 +0200

    counting

--- week7/favorites.py
+++ week7/favorites.py
@@ -1,12 +1,15 @@
 import csv
   
-titles = set()
+titles = {}
 
 with open("favorites.csv", "r") as file:
     reader = csv.DictReader(file)
     for row in reader:
         title = row["title"].strip().lower()
-        titles.add(title)
+        if title in titles:
+            titles[title] += 1
+        else:
+            titles[title] = 1 # start counting at 1 as you see the first title.
 
 for title in sorted(titles):
-    print(title)
+    print(title, titles[title])
