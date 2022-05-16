7d376f87bee9b55218352a46f1b05d23425893c0
Thien Ly Buu Huynh <45794429+xiunhoxiu@users.noreply.github.com>
2022-05-16 11:49:00 +0200

    anonymous function lambda

--- week7/favorites.py
+++ week7/favorites.py
@@ -11,5 +11,6 @@ with open("favorites.csv", "r") as file:
         titles[title] += 1 # start counting from 1 as you see the first title.
 
 # sorted in alphabetical order, sorted by key. reverse
+# lambda anonymous function
 for title in sorted(titles, key=lambda title: titles[title], reverse=True):
     print(title, titles[title])
