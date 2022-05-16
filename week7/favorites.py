6e95f716dd9bb6885ef352444dd79f3d8054f799
Thien Ly Buu Huynh <45794429+xiunhoxiu@users.noreply.github.com>
2022-05-16 11:47:39 +0200

    anonymous function

--- week7/favorites.py
+++ week7/favorites.py
@@ -1,15 +1,15 @@
 import csv
   
-titles = {}
+titles = {}  #dictionary
 
 with open("favorites.csv", "r") as file:
     reader = csv.DictReader(file)
     for row in reader:
         title = row["title"].strip().lower()
-        if title in titles:
-            titles[title] += 1
-        else:
-            titles[title] = 1 # start counting at 1 as you see the first title.
+        if not title in titles:
+            titles[title] = 0
+        titles[title] += 1 # start counting from 1 as you see the first title.
 
-for title in sorted(titles):
+# sorted in alphabetical order, sorted by key. reverse
+for title in sorted(titles, key=lambda title: titles[title], reverse=True):
     print(title, titles[title])
