2652d584a0aaea6e07b1d182fc327aa5c44331d7
Thien Ly Buu Huynh <45794429+xiunhoxiu@users.noreply.github.com>
2022-05-23 11:25:33 +0200

    changes

--- week6/meow.py
+++ week6/meow.py
@@ -3,13 +3,49 @@ print("meow")
 print("meow")
 print("meow")
 
-# better version
-for i in range(3):
+
+# sligtly better - but not reusaable
+for i in [0,1,2]:
     print("meow")
+
+# only meowing 3 three times
+print("meow\n" *3, end="")
+
+# better version, _ is a variable here but not a variable you would not reuse
+for _ in range(3):
+    print("meow")
+
+
+""" 
+
+# using while with user input
+while True:
+    n = int(input("What's n? "))
+    if n > 0:
+        break 
+
+for _ in range(n):
+        print("meow")
+
+"""
+
+
+
+
+
 
 # improve further
 def main():
-        meow(3)  
+    number = get_number()
+    meow(number)  
+
+  
+def get_number():
+    while True:
+        n = int(input("What's n? "))
+        if n > n:
+            break
+    return n
 
 def meow(n):
     for i in range(n):
