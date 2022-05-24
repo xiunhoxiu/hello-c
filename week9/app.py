edd7e2e3e827c0d7522e783e6f2dad01282457dd
Thien Ly Buu Huynh <45794429+xiunhoxiu@users.noreply.github.com>
2022-05-24 23:23:11 +0200

    modified

--- week9/app.py
+++ week9/app.py
@@ -4,6 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    name = request.args.get("name")
+    return render_template("index.html", name_of_person=name)
 
 
