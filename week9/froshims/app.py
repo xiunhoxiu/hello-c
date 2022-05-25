d4a8c8d53733f3096a09dcae640fcb2fb70e11eb
Thien Ly Buu Huynh <45794429+xiunhoxiu@users.noreply.github.com>
2022-05-25 21:08:01 +0200

    modified in templates folder

--- week9/froshims/app.py
+++ week9/froshims/app.py
@@ -3,15 +3,22 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 
 
+SPORTS = [
+    "Basketball",
+    "Soccer",
+    "Ultimate Frisbee"
+]
+
+
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("index.html", sports=SPORTS)
 
 @app.route("/register", methods=["POST"])
 def register():
 
     # validate submission
-    if not request.form.get("name") or request.form.get("sport") not in ["Basketball", "Soccer", "Ultimate Frisbee"]:
+    if not request.form.get("name") or request.form.get("sport") not in SPORTS:
         return render_template("failure.html")
         
     # Confirm registration
