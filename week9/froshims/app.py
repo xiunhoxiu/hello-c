ed8886f9efdef3166316243f80a3e3f6e5c504a9
Thien Ly Buu Huynh <45794429+xiunhoxiu@users.noreply.github.com>
2022-05-26 11:00:23 +0200

    validation and registrants

--- week9/froshims/app.py
+++ week9/froshims/app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+REGISTRANTS = {}
 
 SPORTS = [
     "Basketball",
@@ -20,7 +21,25 @@ def register():
     # validate submission
     if not request.form.get("name") or request.form.get("sport") not in SPORTS:
         return render_template("failure.html")
-        
+            
+    # Validate name
+    name = request.form.get("name")
+    if not name:
+        return render_template("error.html", message="Missing name")
+
+    # Validate sport
+    sport = request.form.get("sport")
+    if not sport:
+        return render_template("error.html", message="Missing sport")
+    if sport in SPORTS:
+        return render_template("error.html", message="Invalid sport")
+
+    # Registrant
+    REGISTRANTS[name] = sport
+
     # Confirm registration
     return render_template("success.html")
 
+@app.route("/registrants")
+def registrants():
+    return render_template("registrants.html", registrants=REGISTRANTS)
