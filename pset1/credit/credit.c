32b8cd3a8e6af180b1b63ebb70ddb0e8fe331862
Thien Ly Buu Huynh <45794429+xiunhoxiu@users.noreply.github.com>
2022-06-12 23:59:26 +0200

    modified

--- pset1/credit/credit.c
+++ pset1/credit/credit.c
@@ -5,15 +5,26 @@
 
 long get_credit(void);
 int get_length(long credit_card_number);
-long multiply(long credit_card_number, int length_of_number);
+long multiply(long credit_card_number, int second_last, int first_last);
 
 
 int main(void) 
 {
     long credit_card_number = get_credit();
     int length_of_number = get_length(credit_card_number);
-    long multiplication = multiply(credit_card_number, length_of_number);
     
+    int second_last = floor(length_of_number / 2);
+    int first_last = ceil((double)length_of_number/2);
+
+    printf("length second last digits = %i\n", second_last);
+    printf("length first last digits = %i\n", first_last);
+
+    long multiplication = multiply(credit_card_number, second_last, first_last);
+    
+
+    
+
+
     long  even1, even2, even3, even4, even5, even6, even7, even8;
     even1 = ((credit_card_number % 10)/1);
     even2 = ((credit_card_number % 1000)/100);
@@ -45,30 +56,24 @@ long get_credit(void)
 int get_length(long credit_card_number)
 {
     int count = 0, second_last, first_last; 
-    
+
     do{ 
         credit_card_number = credit_card_number / 10; 
         count++; 
     }while (credit_card_number != 0);
 
     printf("length of the actual card number: %i\n", count);
-    
-    second_last = floor(count / 2);
-    first_last = ceil((double)count/2);
 
-    printf("length second last digits = %i\n", second_last);
-    printf("length first last digits = %i\n", first_last);
-
-    return second_last;
+    return count;
 }
 
-long multiply(long credit_card_number, int length_of_number)
+long multiply(long credit_card_number, int second_last, int first_last)
 {
     long second_to_last_digit;
 
-    for (int i; i < length_of_number; i++)
+    for (int i = 0; i < second_last; i++)
     {
-        second_to_last_digit = (credit_card_number % (long) pow(100,i+1))/ (long) pow(10, 2*i+1);
+        second_to_last_digit = (credit_card_number % (long) pow(100,i+1))/ (long) pow(10, 2*i+1) * 2;
         printf("%ld ", second_to_last_digit);
     }
     
